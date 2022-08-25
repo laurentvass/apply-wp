47320f9790f636835f78f0c7adc2987bc2cfc625
laurentvass <laurentvass@gmail.com>
2022-08-25 15:39:24 +0200

    creation header, footer et sidebar

--- wp-content/themes/laurentvassal/index.php
+++ wp-content/themes/laurentvassal/index.php
@@ -1,11 +1,3 @@
-/*
-Theme Name: Laurent Vassal
-Author: the Laurent Vassal
-Description: Ceci est mon thème pour le test de recrutement pour le Département de la Manche
-Version: 1.0
-License: GNU General Public License v2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: laurentvassal
-This theme, like WordPress, is licensed under the GPL.
-Use it to make something cool, have fun, and share what you've learned with others.
-*/
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
